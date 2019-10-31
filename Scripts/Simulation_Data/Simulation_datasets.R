5ef60d62a3659d34498ea46288c4e91233c68b82
Qianhui Huang <42647550+qianhuiSenn@users.noreply.github.com>
2019-10-31 14:41:49 -0400

    Add files via upload

--- Scripts/Simulation_Data/Simulation_datasets.R
+++ Scripts/Simulation_Data/Simulation_datasets.R
@@ -1,7 +1,7 @@
 # Required Library
 library(splatter)
 set.seed(1234)
-# Simulation data 1 in manuscript
+# 1. Simulation data 1 in manuscript
 params.groups <- newSplatParams(batchCells = 2000, nGenes = 4000)
 
 sim1 <- splatSimulateGroups(params.groups, group.prob = c(0.1, 0.3, 0.3, 0.1, 0.2), de.prob = c(0.3, 0.2, 0.2, 0.3, 0.4),
@@ -23,7 +23,7 @@ write.csv(truth,"truth_assay1.csv")
 # Save global R object
 saveRDS(sim1,"splatterSim1.RDS")
 
-# simulation data 2 in manuscript
+# 2. simulation data 2 in manuscript
 params.groups <- newSplatParams(batchCells = 2000, nGenes = 10000)
 sim2 <- splatSimulateGroups(params.groups, group.prob = c(1/2, 1/4, 1/8, 1/16, 1/32, 1/64, 10/1024, 4/1024, 2/1024),
                            dropout.shape=-0.5, dropout.mid=1,
@@ -43,7 +43,7 @@ write.csv(truth,"truth_assay2.csv")
 # Save global R object
 saveRDS(sim2,"splatterSim2.RDS")
 
-# simulation data with different de scales in manuscript
+# 3. simulation data with different de scales in manuscript
 scale_list <- list(c(0.1, 0.3, 0.1, 0.3, 0.2), c(0.3, 0.5, 0.3, 0.5, 0.4), c(0.5, 0.7, 0.5, 0.7, 0.6), c(0.7, 0.9, 0.7, 0.9, 0.8))
 name_list <- list('Low', 'Low_Moderate', ' Moderate', 'High')
 
@@ -65,10 +65,28 @@ sim_run <- function(scale, name){
   write.csv(raw, paste("raw_assay", name, '.csv', sep = ''))
   write.csv(truth,paste("true_assay", name, '.csv', sep = ''))
   # save simulation data
-  saveRDS(sim,paste("splatterSim2", name, ".RDS", sep = ''))
+  saveRDS(sim,paste("splatterSim3", name, ".RDS", sep = ''))
 }
 
 for (i in c(1:4)){
   sim_run(scale_list[[i]], name_list[[i]])
 }
 
+# 4. Simulation data in time and memory comparison
+RESULT_DIR <- 'path/to/result'
+batchCells <- c(5000, 10000, 15000, 20000, 25000, 50000)
+for (i in batchCells){
+  params.groups <- newSplatParams(batchCells = i, nGenes = 20000)
+  sim <- splatSimulateGroups(params.groups, group.prob = c(0.2, 0.2, 0.2, 0.2, 0.2),
+                             dropout.shape=-0.5, dropout.mid=1,
+                             dropout.type="experiment", verbose = F)
+  #extract assay from each simulations
+  truth <- assays(sim)[["TrueCounts"]]
+  raw <- assays(sim)[["counts"]]
+  cell_info <- colData(sim)
+  cell_info <- data.frame(cell_info[, c(1, 3)], row.names = 1)
+  saveRDS(true, file = file.path(RESULT_DIR, paste('true_simulation_count',i,'Cell.rds', sep = '_')))
+  saveRDS(raw, file = file.path(RESULT_DIR, paste('raw_simulation_count',i,'Cell.rds', sep = '_')))
+  write.csv(cell_info,paste("cell_info_sim", name, '.csv', sep = ''))
+  saveRDS(sim,paste("splatterSim4_", i, ".RDS", sep = ''))
+}
