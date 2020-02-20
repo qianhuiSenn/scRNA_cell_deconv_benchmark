df858c791d8d3e8120a189dc41b122c7aaf2d7d1
Qianhui Huang <42647550+qianhuiSenn@users.noreply.github.com>
2020-02-19 23:20:58 -0500

    Update garnett_sample_usage.R

--- Scripts/garnett_sample_usage.R
+++ Scripts/garnett_sample_usage.R
@@ -14,17 +14,54 @@ reference_count <- readRDS(file = file.path(DATA_DIR, 'ref_count.rds'))
 gene_id <- row.names(reference_count)
 fd <- data.frame(gene_id, gene_id, row.names = 1)
 colnames(fd) <- c('gene_short_name')
-pd <- readRDS(file = file.path(DATA_DIR, 'ref_meta.rds'))
-reference_cds <- newCellDataSet(reference_count, phenoData = new("AnnotatedDataFrame", data = pd),
+reference_cds <- newCellDataSet(reference_count,
                                 featureData = new("AnnotatedDataFrame", data = fd),
                                 lowerDetectionLimit = 0.5,
                                 expressionFamily = negbinomial.size())
 reference_cds <- estimateSizeFactors(reference_cds)
 
-marker_file_path <- file.path(DATA_DIR, 'celltype_gene_marker.txt')
+marker_file_path <- file.path(DATA_DIR, 'celltype_gene_marker.txt') #if available
 
+##alternatively if not##
+##########DE Table format##########
+## cluster   gene  ###
+## Group1   GeneA ###
+## Group1   GeneB ###
+## ......  ...... ###
+#####################
+#####how to make a marker txt from a output of DE table#####
+marker <- readRDS(file = "path/to/your/DE/table.rds")
+fileConn <- file("celltype_gene_marker.txt")
+group_list <- unique(marker$cluster)
+cell_num <- length(group_list)
+temp <- NULL
+for(i in 1:cell_num){
+  gene_list <- marker[which(marker$cluster == group_list[i]), ]$gene
+  anno1 <- paste('>', group_list[i], sep = '')
+  anno2 <- paste("expressed: ", paste(gene_list, collapse=","), sep = '')
+  temp <- c(temp, anno1, anno2)
+}
+writeLines(temp, fileConn)
+close(fileConn)
 # suggestion: do marker check to remove or add gene
-# marker_check <- check_markers(reference_cds, marker_file_path, db = 'none')
+marker_check <- check_markers(reference_cds, marker_file_path, db = 'none')
+gene_exclude <- marker_check[which(marker_check$summary != "Ok"), ]$gene_id
+marker_after_exclude <- marker[which(!(marker$gene %in% gene_exclude)), ]
+
+fileConn <- file("abc.txt")
+group_list <- unique(marker_after_exclude$cluster)
+cell_num <- length(group_list)
+temp <- NULL
+for(i in 1:cell_num){
+  gene_list <- marker_after_exclude[which(marker_after_exclude$cluster == group_list[i]), ]$gene
+  anno1 <- paste('>', group_list[i], sep = '')
+  anno2 <- paste("expressed: ", paste(gene_list, collapse=","), sep = '')
+  temp <- c(temp, anno1, anno2)
+}
+writeLines(temp, fileConn)
+close(fileConn)
+
+###make classifier
 reference_classifier <- train_cell_classifier(cds = reference_cds,
                                               marker_file = marker_file_path,
                                               db='none',
